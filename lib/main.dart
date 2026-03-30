0ab51a3bc55e42b20acb2c70acd79488cf47bed9
SAMUEL ALVES PINHO <samuel.pinho3@senaisp.edu.br>
2026-03-30 11:14:05 -0300

    x

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:vini_portifolio/pagamento.dart';
+import 'package:url_launcher/link.dart';
+import 'package:vini_portifolio/tela2.dart';
 
 void main() {
   runApp(const MyMaterialApp());
@@ -36,38 +36,59 @@ class TelaInicial extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color(0XFF5B62B4),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
             Text("Cara um teste sobre isoo!"),
             Text("Abaixo terá uma foto minha não sei o pq"),
             Icon(Icons.arrow_downward),
+      
            ClipRRect(
             borderRadius: BorderRadius.circular(1000), // Raio igual à metade da largura/altura
-              child: Image.network(
-                "https://wallpapers.com/images/hd/anime-meme-pfp-shiba-dog-naruto-akatsuki-qg4jwiz9wanor4vf.jpg",
+              child: Image.asset(
+                "assets/Gemini_Generated_Image_80s4ew80s4ew80s4.png",
                 width: 300,
                 height: 300,
+                
                 fit: BoxFit.cover, // Garante que a imagem preencha o círculo
               ),
             ),
-            Container(child: Center(child: Column(
-              children: [
-                Text('''Caro leitor, receba estas linhas tortas,
-                      Como chaves enferrujadas para velhas portas.
-                      Não trago verdades, nem certezas absolutas,
-                      Apenas ecos das minhas próprias lutas.
-                      Sinta o peso do silêncio que a página esconde,
-                      E a urgência da alma que sempre responde.
-                      A poesia é o relâmpago, o sopro, a luz,
-                      O fardo leve que a escrita me traduz.
-                      Se o verso te tocar, ou te fizer pensar,
-                      Terá valido a pena o ato de inventar.
-                      Pois a magia não está no que escrevi,
-                      Mas no espelho que formamos, tu e eu aqui.''')
-              ],
-            )))
-          ]     
+            Container(
+              width: 400,
+              padding: const EdgeInsets.all(30.0),
+              
+             child:
+                Text("Caro leitor, receba estas linhas portas,  Não trago verdades, nem certezas absolutas,  Apenas ecos das minhas próprias lutas.   Sinta o peso do silêncio que a página esconde,  E a urgência da alma que sempre responde. A poesia é o relâmpago, o sopro, a luz,   O fardo leve que a escrita me traduz. Se o verso te tocar, ou te fizer pensar,  Terá valido a pena o ato de inventar.  Pois a magia não está no que escrevi, Mas no espelho que formamos, tu e eu aqui.")
+                
+            ),
+                  Text("Projetos"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                TextButton(onPressed: (){
+               Navigator.push(context, MaterialPageRoute(builder:(context) => TelaInicial()));
+            }, child: Text("não sei")),
+            
+            TextButton(onPressed: (){
+               Navigator.push(context, MaterialPageRoute(builder:(context) => TelaInicial()));
+            }, child: Text("não sei")),
+          
+            TextButton(onPressed: (){
+               Navigator.push(context, MaterialPageRoute(builder:(context) => TelaInicial()));
+            }, child: Text("não sei")),
+              
+              ]
+              ),
+
+                    Tela2(url_imagem: "assets/Linkedin.jpg", nome_rede: "Linkedin", link: "https://www.linkedin.com"),
+                    Tela2(url_imagem: "assets/GitHub.png", nome_rede: "GitHub", link: "https://github.com/Samueldog-rgb"),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => TelaInicial()));
+                      },
+                      child: Text("Meus projetos"),
+                    )
+          ]
         ),
       ),
     );
---
+++ lib/tela2.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Tela2 extends StatelessWidget {
+  String url_imagem;
+  String nome_rede;
+  String link;
+  Tela2({super.key, required this. url_imagem, required this.nome_rede , required this.link});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        child:Container(
+        width: MediaQuery.of(context).size.width * 0.2,
+        height: MediaQuery.of(context).size.height * 0.2,
+        child: Column(
+          children:[
+            Image.network(url_imagem, width: 100, height: 100),
+            Text(nome_rede),
+          ]
+        )
+      ),
+
+      onTap: ()async {
+        
+        Uri url_convertido = Uri.parse(link);
+
+        if(await canLaunchUrl(url_convertido)){
+          await launchUrl(url_convertido);
+        }
+ 
+      },
+    );
+  }
+}
